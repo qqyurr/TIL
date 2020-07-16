fce32c4f5ffac5e9d1701c6e84c8072fbca8404b
qqyurr <byyulli16@gmail.com>
2020-07-16 17:43:15 +0900

    Initial commit

---
+++ file_practice/rename_files.py
@@ -0,0 +1,12 @@
+import os
+
+
+os.chdir(r'C:\Users\aclass\Desktop\file_practice\dummy')
+
+filenames = os.listdir('.')
+
+for filename in filenames:
+    new_filename = filename.replace('SAMSUNG_','SSAFY_')
+    os.rename(filename, new_filename)
+
+
